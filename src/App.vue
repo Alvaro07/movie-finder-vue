545fe5e2341567d0b2b8ec6542cf7356776092af
Alvaro AVGS. Gonzalez Serrano <alvaro.gonzalez@stkproj.com>
2019-05-16 12:16:34 +0200

    first changes

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -20,9 +17,15 @@
   padding: 30px;
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: #42b983;
+    transition: 0.3s all ease;
+
+    &:hover {
+      color: #0f673f;
+    }
     &.router-link-exact-active {
-      color: #42b983;
+      color: #2c3e50;
+      cursor: default;
     }
   }
 }
---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // publicPath: "movie-finder-vue",
+  css: {
+    loaderOptions: {
+      sass: {
+        data: `
+        @import "@/styles/settings/_variables.scss";
+        @import "@/styles/settings/_mixins.scss";
+      `
+      }
+    }
+  }
+};
