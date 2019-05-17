003ee5912036db67a2c4efe81783a1a9d7a408ee
Alvaro AVGS. Gonzalez Serrano <alvaro.gonzalez@stkproj.com>
2019-05-17 13:48:33 +0200

    Add styles for homepage

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,14 @@
 <template>
-  <div id="app">
-    
+  <div id="app" class="app">
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #42b983;
-    transition: 0.3s all ease;
+@import "styles/index";
 
-    &:hover {
-      color: #0f673f;
-    }
-    &.router-link-exact-active {
-      color: #2c3e50;
-      cursor: default;
-    }
-  }
+.app {
+  min-height: 100%;
+  background-image: linear-gradient(120deg, #fdfbfb 0%, #ebedee 100%);
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,12 @@
 <template>
-  <div class="home">
-    Home
-  </div>
+  <section class="home">
+    <router-link tag="figure" to="/films" class="home__figure home__figure--films">
+      <h2 class="home__figure__title">Films</h2>
+    </router-link>
+    <router-link tag="figure" to="/series" class="home__figure home__figure--series">
+      <h2 class="home__figure__title">Series</h2>
+    </router-link>
+  </section>
 </template>
 
 <script>
@@ -10,4 +15,111 @@ export default {
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.home {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+
+  @include mediaDesktop {
+    flex-direction: initial;
+  }
+
+  &__figure {
+    flex: 1;
+    cursor: pointer;
+    background-size: cover;
+    background-position: center center;
+    position: relative;
+    z-index: 1;
+    transition: all 0.3s ease;
+
+    // Hover effect
+
+    &::after {
+      content: "";
+      position: absolute;
+      left: 0;
+      right: 0;
+      height: 5px;
+      background-color: $pink;
+      transform: scaleX(0);
+      transition: transform 0.2s ease-in;
+    }
+
+    &:hover {
+      opacity: 0.9;
+      .home__figure__title {
+        color: $pink;
+      }
+    }
+
+    &::after {
+      bottom: 0;
+      transform-origin: center left;
+    }
+
+    &:hover::after {
+      transform-origin: center right;
+      transform: scaleX(1);
+    }
+
+    // Figure difference
+
+    &--films {
+      --film-image: url("../assets/star-wars.webp");
+      background-image: var(--film-image);
+
+      @include mediaDesktop {
+        --film-image: url("../assets/star-wars-large.jpg");
+      }
+
+      .home__figure__title {
+        right: 20px;
+        top: 10px;
+
+        @include mediaDesktop {
+          right: 30px;
+          top: initial;
+          bottom: 30px;
+        }
+      }
+    }
+
+    &--series {
+      --series-image: url("../assets/got.jpeg");
+      background-image: var(--series-image);
+
+      @include mediaDesktop {
+        --series-image: url("../assets/got-large.jpg");
+      }
+
+      .home__figure__title {
+        left: 20px;
+        bottom: 10px;
+
+        @include mediaDesktop {
+          left: inherit;
+          right: 30px;
+          bottom: 30px;
+        }
+      }
+    }
+
+    &__title {
+      --typo-size: 5rem;
+
+      font-family: $Luckiest;
+      color: white;
+      font-weight: 400;
+      font-size: var(--typo-size);
+      position: absolute;
+      transition: all 0.3s ease;
+
+      @include mediaTablet {
+        --typo-size: 7rem;
+      }
+    }
+  }
+}
+</style>
