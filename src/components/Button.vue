6c7face7d50cf3bab2389e3e2cd9fb74828aed4c
Alvaro AVGS. Gonzalez Serrano <alvaro.gonzalez@stkproj.com>
2019-05-18 13:01:57 +0200

    Add input and button componnet

---
+++ src/components/Button.vue
@@ -0,0 +1,21 @@
+<template>
+  <button>
+    <font-awesome-icon v-if="this.iconClass" :icon="this.iconClass"/>
+    {{ text }}
+  </button>
+</template>
+<script>
+export default {
+  name: "v-button",
+  props: {
+    text: {
+      type: String
+    },
+    iconClass: {
+      type: String
+    }
+  }
+};
+</script>
+<style lang="scss">
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// Fontawesome icons
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faHome, faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faHome, faSearch);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
+
+// Develop mode
 Vue.config.productionTip = false
 Vue.config.devtools = true;
 
